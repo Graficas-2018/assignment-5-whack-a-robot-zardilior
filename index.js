05bfa632fb65c448e37d0f1d221140a8ddb77951
jose estremadoyro <joseenrique@MacBook-Pro-3.local>
2018-10-05 08:47:58 -0500

    Intial lightning

--- index.js
+++ index.js
@@ -2,11 +2,67 @@ app = {
     holeRows : 5,
     holeColumns : 5,
     moles : [],
+    holes : [],
     setup : function(){
-        // Initialize canvas
-        // Initialize scene,camera and renderer
+        console.log(app);
+        // Initialize scene,camera,renderer and lights
+        
+            // Scene
+            app.scene = new THREE.Scene();
+            app.width = window.innerWidth;
+            app.height = window.innerHeight;
+
+            // Renderer
+            app.renderer = new THREE.WebGLRenderer({
+                antialias:true,
+                canvas:document.getElementById('canvas')
+            });
+            app.renderer.setSize(app.width,app.height*.8);
+
+            // Camera
+            app.camera = new THREE.PerspectiveCamera(45,app.width/app.height*.8,.1,20000);
+            app.camera.position.set(0,-70,50);
+            app.camera.lookAt(0,-20,0);
+            app.scene.add(app.camera);
+
+            // Lights
+            app.hemisphereLight = new THREE.HemisphereLight(0xffffff,0x000000);
+            app.scene.add(app.hemisphereLight);
+            app.pointLight = new THREE.PointLight(0xffff00);
+            app.pointLight.position.set(0,0,15);
+            app.scene.add(app.pointLight);
+
+        // Create plane
+            var geometry = new THREE.PlaneGeometry( 100, 100);
+            var material = new THREE.MeshPhongMaterial( {
+                color: 0xffffff, 
+                side: THREE.DoubleSide,
+            });
+            app.plane = new THREE.Mesh( geometry, material );
+            app.scene.add(app.plane );
+
+
         // LoadModel
         // Draw holes
+            var circleRadius = 100/Math.max(app.holeColumns+1,app.holeRows+1)/4;
+            var geometry = new THREE.CircleGeometry( 
+                circleRadius,
+                32
+            );
+            var material = new THREE.MeshPhongMaterial( { color: 0x000033 } );
+            var tileInitial = -50+circleRadius*4;
+            var tileLength = circleRadius*4;
+            for(let i=0;i<app.holeColumns;i++){
+               for(let j=0;j<app.holeRows;j++){
+                    var hole = new THREE.Mesh(geometry,material);
+                    hole.position.set(tileInitial+i*tileLength,tileInitial+j*tileLength,1);
+                    console.log(hole.position);
+                    app.holes.push(hole);
+                    app.scene.add(hole);
+               }
+            }
+        // Render scene
+            setTimeout(function(){app.renderer.render(app.scene,app.camera)},300);
     },
     start : function(){
         // Generate random numbers array filled with [timeoutTime,holex,holey,deletionTimeout]
@@ -14,13 +70,13 @@ app = {
     },
     restart : function(){
         // Delete all moles
-        this.mole.deleteAll();
+        app.mole.deleteAll();
 
         // Reset points
-        this.points.reset();
+        app.points.reset();
 
         //Start game
-        this.start()
+        app.start()
     },
     points : {
         value : 0,
@@ -36,7 +92,7 @@ app = {
             this.value=0;
             // Draw points
             this.draw();
-        }
+        },
         draw : function(){
             // text element draw
             this.text.textContent = this.value;
@@ -55,13 +111,12 @@ app = {
         deathAnimation : function(){
             // Rotate on the side
             // Run walk animation, each time slower until stop and remove
-        }
+        },
         deleteAll : function(){
             // for each mole in moles
                 // delete mole
         }
     }
 }
-document.on('DOMContentLoaded', function(){
-    app.setup();    
-});
+
+document.addEventListener('DOMContentLoaded', app.setup);
